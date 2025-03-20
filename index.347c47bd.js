0e5f1a502b24ecfa817d0d959051cbc5eaf06d85
Tetiana Makarevych <tanyamakarevich81@gmail.com>
2025-03-20 15:33:57 +0200

    make build

---
+++ index.347c47bd.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".phone__dropdown"),n=document.querySelector(".icon--phone");document.querySelector(".phone__menu"),n.addEventListener("contextmenu",function(n){n.preventDefault(),e.classList.toggle(".phone--active")});
+//# sourceMappingURL=index.347c47bd.js.map
